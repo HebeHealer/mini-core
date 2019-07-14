d02b96f188460b65f7abc67e7c8cb40687c1b85c
HebeHealer <HebeHealer@xgimi.com>
2019-07-14 21:40:51 +0800

    feat(初始化项目)：搭建vue+iview+小程序

---
+++ config/router.js
@@ -0,0 +1,33 @@
+/**
+ * CMD 导出方式
+ */
+
+module.exports = [
+  {
+    "pagePath": "pages/index/index",
+    "text": "首页",
+    "active": "index",
+    "icon": "homepage",
+    "iconPath": "/assets/imgs/icon_tabbar.png",
+    "selectedIconPath": "/assets/imgs/icon_tabbar.png",
+    "auth": 0
+  },
+  {
+    "pagePath": "pages/info/index",
+    "text": "发现",
+    "active": "info",
+    "icon": "group",
+    "iconPath": "/assets/imgs/icon_tabbar.png",
+    "selectedIconPath": "/assets/imgs/icon_tabbar.png",
+    "auth": 0
+  },
+  {
+    "pagePath": "pages/user/index",
+    "text": "我的",
+    "active": "user",
+    "icon": "mine",
+    "iconPath": "/assets/imgs/icon_tabbar.png",
+    "selectedIconPath": "/assets/imgs/icon_tabbar.png",
+    "auth": 0
+  }
+]
